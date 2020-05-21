ef8fc68178afb2cbb21805138e81816a03421432
Niall <23546468@edgehill.ac.uk>
2020-05-21 23:26:17 +0100

    Create sqlSetup.scala

---
+++ sqlSetup.scala
@@ -0,0 +1,36 @@
+CREATE DATABASE GoogleLocal;
+USE GoogleLocal;
+
+CREATE TABLE reviews (
+placeid CHAR(21),
+userid CHAR(21),
+rating CHAR(21),
+reviewername VARCHAR(30),
+reviewtext STRING,
+category varchar(20),
+reviewtime VARCHAR(15)
+) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+
+CREATE TABLE places (
+placeid CHAR(21),
+placename VARCHAR(40),
+pricerange VARCHAR(3),
+address VARCHAR(40),
+address VARCHAR(40),
+hours STRING,
+phone VARCHAR(15),
+closed VARCHAR(5)
+) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+
+CREATE TABLE users (
+userid CHAR(21),
+username VARCHAR(40),
+jobs VARCHAR(40),
+currentplace VARCHAR(30),
+previousplace VARCHAR(30),
+education STRING
+) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+
+load data inpath '/home/cloudera/cw2/reviews.csv/' into table reviews;
+load data inpath '/home/cloudera/cw2/places.csv/' into table places;
+load data inpath '/home/cloudera/cw2/users.csv/' into table users;
